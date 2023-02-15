b0be639203145fd79fb6903d2cc753dd23d70149
Josh Feuerstein <jtf82@cornell.edu>
2023-02-15 17:06:07 -0500

    added button to homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {/*TODO: Test out your buttons here!*/}
+      <Button></Button>
       <StatusBar style="auto" />
     </View>
   );
