755672a46642dc43d072472732ac5416088d5b98
Josh Feuerstein <jtf82@cornell.edu>
2023-02-15 18:21:41 -0500

    made main work temp

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,6 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       {/*TODO: Test out your buttons here!*/}
-      <Button></Button>
       <StatusBar style="auto" />
     </View>
   );
