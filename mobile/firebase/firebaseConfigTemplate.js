603461c2587683c563ee3a25032e011050631a99
Josh Feuerstein <jtf82@cornell.edu>
2023-03-01 16:42:37 -0500

    installed Firebase SDK adn Firebase Admin SDK. Prepared for Authentication to be implemented.

--- mobile/firebase/firebaseConfigTemplate.js
+++ mobile/firebase/firebaseConfigTemplate.js
@@ -9,14 +9,7 @@ import { initializeApp } from 'firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: 'api',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  
 };
 
 const app = initializeApp(firebaseConfig);
