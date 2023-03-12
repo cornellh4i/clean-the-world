9643d21f2223fc7d9ab6c7d6ced710db62943e5c
hanahhleee <hel33@cornell.edu>
2023-03-12 00:57:19 -0500

    Fixed phone auth flow, error messages, styling

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,8 @@
 import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
-import { RecaptchaVerifier, signInWithPhoneNumber, PhoneAuthProvider, signInWithCredential, getAuth } from "firebase/auth";
-// import OtpInput from "otp-input-react";
-// import PhoneInput from "react-phone-input-2";
-import { FirebaseRecaptchaVerifier, FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { PhoneAuthProvider, signInWithCredential } from "firebase/auth";
+import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 import { getApp } from 'firebase/app';
 import { auth } from "../mobile/firebase/firebaseConfig.js"
 
@@ -18,50 +16,36 @@ export default function App() {
   const recaptchaVerifier: any = useRef(null);
   const attemptInvisibleVerification = false;
   const appVerificationDisabledForTesting = false;
+  const textInput: any = useRef(null);
 
   const app = getApp();
 
+
   const firebaseConfig = app ? app.options : undefined;
 
-  // function generateRecaptcha() {
-  //   if (!window.recaptchaVerifier) {
-  //     window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-  //       'size': 'invisible',
-  //       'callback': () => {
-  //         // reCAPTCHA solved, allow signInWithPhoneNumber.
-  //         onSignIn();
-  //       },
-  //       "expired-callback": () => { },
-  //     }, auth);
-  //   }
-  // }
+  // TO DO
+  // make phone authentication a separate component
+  // timer for otp verification? resend otp?
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <>
-        {/* <FirebaseRecaptchaVerifierModal
-          ref={recaptchaVerifier}
-          firebaseConfig={firebaseConfig}
-          attemptInvisibleVerification={attemptInvisibleVerification}
-          appVerificationDisabledForTesting={appVerificationDisabledForTesting}
-        /> */}
-        {/* <div id="recaptcha-container"></div> */}
-        {(<FirebaseRecaptchaVerifierModal
+        {<FirebaseRecaptchaVerifierModal
           ref={recaptchaVerifier}
           firebaseConfig={firebaseConfig}
           attemptInvisibleVerification={attemptInvisibleVerification}
           appVerificationDisabledForTesting={appVerificationDisabledForTesting}
         />
-        )}
+        }
         {user ? (
-          <Text>Login Successful</Text>
+          <Text style={styles.promptText}>Login Successful!</Text>
         ) : (
           <View style={styles.container}>
-            <Text>Welcome to Clean the World!</Text>
+            <Text style={styles.titleText}>Welcome to Clean the World!</Text>
             {!showOTP ? (
               <>
-                <Text style={{ marginTop: 20 }}>Verify your phone number</Text>
+                <Text style={styles.promptText}>Verify your phone number</Text>
                 <TextInput
                   style={{ marginVertical: 10, fontSize: 17 }}
                   placeholder="+1 999 999 9999"
@@ -71,43 +55,46 @@ export default function App() {
                   textContentType="telephoneNumber"
                   onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
                 />
-                <Button
+                <TouchableOpacity style={styles.smallButton}
                   onPress={async () => {
                     setLoading(true);
                     try {
                       const phoneProvider = new PhoneAuthProvider(auth);
-                      console.log(verificationId);
                       const newVerificationId = await phoneProvider.verifyPhoneNumber(
-                        // "+12039062861",
                         phoneNumber,
                         recaptchaVerifier.current
                       );
-                      console.log(newVerificationId);
                       setVerificationId(newVerificationId);
                       setLoading(false);
                       setShowOTP(true);
-                      // alert('Verification code has been sent to your phone.');
+                      textInput.current.clear();
                     }
                     catch (err) {
-                      // if (err == TypeError)
                       setLoading(false);
-                      alert(err);
+                      setShowOTP(false);
+                      if (err == "Error: Cancelled by user")
+                        alert("ReCAPTCHA verification failed")
+                      else alert("Phone number is incorrect or not verified");
                     }
-                  }}
-                  title="Send code via SMS"
-                  color="#9FD4A3"
-                // {loading}
-                />
+                  }}>
+                  <Text style={styles.buttonText}>Send code via SMS</Text>
+                  {/* {loading} */}
+                </TouchableOpacity>
               </>
             ) : (
               <>
-                <Text style={{ marginTop: 20 }}>Enter your OTP</Text>
+                <Text style={styles.promptText}>Enter your OTP</Text>
                 <TextInput
                   style={{ marginVertical: 10, fontSize: 17 }}
                   placeholder="123456"
+                  autoFocus
+                  autoComplete="sms-otp"
+                  keyboardType="number-pad"
+                  textContentType="oneTimeCode"
                   onChangeText={otp => setOtp(otp)}
+                  ref={input => { textInput.current = input }}
                 />
-                <Button
+                <TouchableOpacity style={styles.smallButton}
                   onPress={async () => {
                     try {
                       setLoading(true);
@@ -115,40 +102,49 @@ export default function App() {
                       const userCredential = await signInWithCredential(auth, authCredential);
                       setUser(userCredential);
                       setLoading(false);
-                      // alert('Phone authentication successful')
                     } catch (err) {
                       setLoading(false);
-                      alert(err);
+                      alert("Code is incorrect");
                     }
-                  }
-                  }
-                  title="Verify OTP"
-                  color='#9FD4A3'
-                // {loading}
-                />
+                  }}>
+                  <Text style={styles.buttonText}>Verify OTP</Text>
+                  {/* {loading} */}
+                </TouchableOpacity>
               </>
             )}
           </View>
         )}
       </>
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FEF4E1',
+    backgroundColor: '#fef4e1',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  verifyButton: {
-    flex: 1,
+  smallButton: {
     borderRadius: 8,
     padding: 10,
     margin: 10,
-    backgroundColor: '#9FD4A3',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#9fd4a3',
+    borderColor: '#f0f0f0'
+  },
+  buttonText: {
+    fontFamily: 'Verdana',
+    fontSize: 15,
+    color: '#ffffff'
+  },
+  promptText: {
+    fontFamily: 'Verdana',
+    fontSize: 15
+  },
+  titleText: {
+    padding: 10,
+    fontFamily: 'Verdana',
+    fontSize: 15
   },
 });
