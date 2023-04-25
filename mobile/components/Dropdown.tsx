6db44c9482fce566ef0075b4e8a5af6b6b669859
Suhani Patel <sjp269@cornell.edu>
2023-04-25 16:29:58 -0400

    completed addition of dropdown component, unable to change background color for some reason

--- mobile/components/Dropdown.tsx
+++ mobile/components/Dropdown.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 function DropdownComponent() {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState('null');
   const [items, setItems] = useState([
     { label: 'Day', value: 'd' },
     { label: 'Month', value: 'm' },
@@ -11,6 +12,11 @@ function DropdownComponent() {
 
   return (
     <DropDownPicker
+
+      containerStyle={styles.container}
+      placeholder='Day'
+      placeholderStyle={styles.placeholder}
+      arrowIconStyle={styles.arrow}
       open={open}
       value={value}
       items={items}
@@ -20,4 +26,24 @@ function DropdownComponent() {
     />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: 91,
+    height: 32,
+    left: 225,
+    top: 184,
+    backgroundColor: '#D9D9D9'
+  },
+  arrow: {
+    backgroundColor: '#D9D9D9',
+
+  },
+  placeholder: {
+    backgroundColor: '#D9D9D9',
+
+
+  }
+});
 export default DropdownComponent;
