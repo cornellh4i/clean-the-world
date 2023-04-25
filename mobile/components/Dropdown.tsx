93c860ca85b6986fcb4e629921f57c4c887bd9e4
Suhani Patel <sjp269@cornell.edu>
2023-04-25 17:44:10 -0400

    changed background color and added state storage

--- mobile/components/Dropdown.tsx
+++ mobile/components/Dropdown.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 function DropdownComponent() {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('null');
+  const [value, setValue] = useState(null);
   const [items, setItems] = useState([
     { label: 'Day', value: 'd' },
     { label: 'Month', value: 'm' },
@@ -12,7 +12,7 @@ function DropdownComponent() {
 
   return (
     <DropDownPicker
-
+      style={styles.bgcolor}
       containerStyle={styles.container}
       placeholder='Day'
       placeholderStyle={styles.placeholder}
@@ -21,6 +21,7 @@ function DropdownComponent() {
       value={value}
       items={items}
       setOpen={setOpen}
+      onChangeValue={value => setValue}
       setValue={setValue}
       setItems={setItems}
     />
@@ -28,13 +29,15 @@ function DropdownComponent() {
 }
 
 const styles = StyleSheet.create({
+  bgcolor: {
+    backgroundColor: '#D9D9D9'
+  },
   container: {
     position: 'absolute',
     width: 91,
     height: 32,
     left: 225,
     top: 184,
-    backgroundColor: '#D9D9D9'
   },
   arrow: {
     backgroundColor: '#D9D9D9',
